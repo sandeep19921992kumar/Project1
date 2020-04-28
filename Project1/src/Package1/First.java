0a2226e67773a55f0cd29593852e7881d10db711
MY PC <MY PC@192.168.1.4>
2020-04-28 22:01:20 +0530

    first project commit and push for testing setup

---
+++ Project1/src/Package1/First.java
@@ -0,0 +1,33 @@
+package Package1;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+
+public class First {
+
+	
+	/*
+	 * public static void main(String[] args) throws InterruptedException { // TODO
+	 * System.setProperty("webdriver.gecko.driver",
+	 * "C:\\Users\\MY PC\\Desktop\\Sandeep\\SoftwaresAndJars\\All_Drivers\\geckodriver.exe"
+	 * ); DesiredCapabilities capability = DesiredCapabilities.firefox();
+	 * capability.setBrowserName("firefox"); capability.setVersion("75.0.0.7398");
+	 * capability.setPlatform(Platform.WINDOWS); WebDriver driver=new
+	 * FirefoxDriver(capability); driver.get("https://www.facebook.com/");
+	 * Thread.sleep(2000); driver.quit();
+	 * 
+	 * }
+	 */
+	 
+	@Test
+	public void m1()
+	{
+		System.out.println("Hello First Test Execute in TestNg-Sucessfully");
+	}
+
+}
---
+++ Project1/src/Package1/scnd.java
@@ -0,0 +1,12 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class scnd {
+	@Test
+	public void m1()
+	{
+		System.out.println("hum ho gy print");
+	}
+
+}
